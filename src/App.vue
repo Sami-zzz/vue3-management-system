cd3aec6081340ef9e66de6bc3bc0dcef57862b13
Sami-zzz <2787621724@qq.com>
2023-05-06 16:54:11 +0800

    feat: 使用normalize.css，配置基本css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <h2>{{ $store.state.name }}</h2>
-  <router-link to="/login">登录</router-link>
-  <router-link to="/main">首页</router-link>
   <router-view></router-view>
-  <el-button type="danger">123</el-button>
 </template>
 
 <script lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,38 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import myRequest from './service'
+import 'normalize.css'
+import './assets/css/index.less'
+
+// import myRequest from './service'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
 app.mount('#app')
 
-interface DataType {
-  data: any
-  returnCode: string
-  success: boolean
-}
+//测试请求
+// interface DataType {
+//   data: any
+//   returnCode: string
+//   success: boolean
+// }
 
-myRequest
-  .request<DataType>({
-    url: '/post',
-    method: 'POST',
-    showLoading: true,
-    interceptors: {
-      requestInterceptor: (config) => {
-        console.log('request单独的请求拦截器')
-        return config
-      }
-    }
-  })
-  .then((res) => {
-    console.log('***********')
-    console.log(res.data)
-    console.log(res.returnCode)
-    console.log(res.success)
-  })
+// myRequest
+//   .request<DataType>({
+//     url: '/post',
+//     method: 'POST',
+//     showLoading: true,
+//     interceptors: {
+//       requestInterceptor: (config) => {
+//         console.log('request单独的请求拦截器')
+//         return config
+//       }
+//     }
+//   })
+//   .then((res) => {
+//     console.log('***********')
+//     console.log(res.data)
+//     console.log(res.returnCode)
+//     console.log(res.success)
+//   })
